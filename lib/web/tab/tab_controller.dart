0b87e222b8775d508b9a0270872ff50376d31f3a
Mike Lewis <mike@updroid.com>
2015-07-22 15:07:53 -0700

    tab: tab controller now gets id info via dom events from the client

--- lib/web/tab/tab_controller.dart
+++ lib/web/tab/tab_controller.dart
@@ -2,6 +2,7 @@ library tab_controller;
 
 import 'dart:html';
 import 'dart:async';
+import 'dart:convert';
 
 import '../mailbox/mailbox.dart';
 import '../tab/container_view.dart';
@@ -18,34 +19,46 @@ abstract class TabController {
   Mailbox mailbox;
   AnchorElement _closeTabButton;
 
-  TabController(this.id, this.col, this.fullName, this.shortName, List menuConfig, [String externalCssPath]) {
-    mailbox = new Mailbox(fullName, id);
+  TabController(this.fullName, this.shortName, List menuConfig, [String externalCssPath]) {
+    // Listen for CustomEvents called CustomDartEvent.
+    EventStreamProvider<CustomEvent> tabIdStream = new EventStreamProvider<CustomEvent>('TabIdEvent');
+    tabIdStream.forTarget(window).first.then((CustomEvent e) {
+      Map detail = JSON.decode(e.detail);
+      id = detail['id'];
+      col = detail['col'];
 
-    registerMailbox();
+      mailbox = new Mailbox(fullName, id);
 
-    TabView.createTabView(id, col, fullName, shortName, menuConfig, externalCssPath).then((tabView) {
-      view = tabView;
+      registerMailbox();
 
-      _closeTabButton = view.refMap['close-tab'];
-      _closeTabButton.onClick.listen((e) => _closeTab());
-      view.closeControlHitbox.onClick.listen((e) => _closeTab());
+      TabView.createTabView(id, col, fullName, shortName, menuConfig, externalCssPath).then((tabView) {
+        view = tabView;
 
-      view.tabHandleButton.onContextMenu.listen((e) {
-        e.preventDefault();
-        List menu = [
-          {'type': 'toggle', 'title': 'Clone', 'handler': _cloneTab},
-          {'type': 'toggle', 'title': 'Move ${col == 1 ? 'Right' : 'Left'}', 'handler': () => _moveTabTo(col == 1 ? 2 : 1)}];
-        ContextMenu.createContextMenu(e.page, menu);
+        _closeTabButton = view.refMap['close-tab'];
+        _closeTabButton.onClick.listen((e) => _closeTab());
+        view.closeControlHitbox.onClick.listen((e) => _closeTab());
+
+        view.tabHandleButton.onContextMenu.listen((e) {
+          e.preventDefault();
+          List menu = [
+            {'type': 'toggle', 'title': 'Clone', 'handler': _cloneTab},
+            {'type': 'toggle', 'title': 'Move ${col == 1 ? 'Right' : 'Left'}', 'handler': () => _moveTabTo(col == 1 ? 2 : 1)}];
+          ContextMenu.createContextMenu(e.page, menu);
+        });
+
+        setUpController();
+        registerEventHandlers();
+
+        // When the content of this tab receives focus, transfer it to whatever is the main content of the tab
+        // (which may or may not be the direct child of view.content).
+        // Also, this is done last as additional view set up may have been done in setUpController().
+        view.tabContent.onFocus.listen((e) => elementToFocus.focus());
       });
-
-      setUpController();
-      registerEventHandlers();
-
-      // When the content of this tab receives focus, transfer it to whatever is the main content of the tab
-      // (which may or may not be the direct child of view.content).
-      // Also, this is done last as additional view set up may have been done in setUpController().
-      view.tabContent.onFocus.listen((e) => elementToFocus.focus());
     });
+
+    // Let UpCom know that we are ready for ID.
+    CustomEvent event = new CustomEvent('TabReadyForId', canBubble: false, cancelable: false);
+    window.dispatchEvent(event);
   }
 
   void makeActive() => view.makeActive();
