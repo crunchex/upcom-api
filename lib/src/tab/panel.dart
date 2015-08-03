e6e7b5ed8fc76dc1d49ff22917e319c48de84a92
Mike Lewis <mike@updroid.com>
2015-08-03 12:51:53 -0700

    panel: added panel class

---
+++ lib/src/tab/panel.dart
@@ -0,0 +1,64 @@
+library upcom_api.lib.src.tab.panel;
+
+import 'dart:async';
+import 'dart:isolate';
+
+import 'package:path/path.dart';
+
+import 'updroid_message.dart';
+import 'tab_mailbox.dart';
+
+abstract class Panel {
+  static const String upcomName = 'upcom';
+
+  static Future main(SendPort interfacesSendPort, List args, Function constructor) async {
+    // Set up the isolate's port pair.
+    ReceivePort isolatesReceivePort = new ReceivePort();
+    interfacesSendPort.send(isolatesReceivePort.sendPort);
+
+    Panel panel = constructor(interfacesSendPort, args);
+
+    await for (var received in isolatesReceivePort) {
+      panel.mailbox.receive(received);
+    }
+  }
+
+  String refName, fullName, shortName, panelPath;
+  int id;
+
+  TabMailbox mailbox;
+
+  Panel(List names, SendPort sendPort, List args) {
+    refName = names[0];
+    fullName = names[1];
+    shortName = names[2];
+
+    panelPath = normalize(args[0]);
+    id = args[1];
+
+    mailbox = new TabMailbox(sendPort, refName, id);
+
+    // Register Panel's event handlers.
+    mailbox.registerMessageHandler('CLOSE_PANEL', _closePanel);
+    mailbox.registerMessageHandler('UPDATE_COLUMN', _updateColumn);
+
+    // Register subclass' event handlers.
+    registerMailbox();
+  }
+
+  void registerMailbox();
+  void cleanup();
+
+  void close() {
+    cleanup();
+  }
+
+  void _closePanel(String msg) {
+    Msg m = new Msg('CLOSE_PANEL', msg);
+    mailbox.relay(upcomName, -1, m);
+  }
+
+  void _updateColumn(String msg) {
+    mailbox.send(new Msg('UPDATE_COLUMN', msg));
+  }
+}
