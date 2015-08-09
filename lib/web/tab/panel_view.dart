42815676cdeac9be9389f870cc7d8a4f26c9e17d
Mike Lewis <mike@updroid.com>
2015-08-09 16:39:51 -0700

    panel: removed close button from panel handle

--- lib/web/tab/panel_view.dart
+++ lib/web/tab/panel_view.dart
@@ -13,8 +13,6 @@ class PanelView extends ContainerView {
     return c.future;
   }
 
-  DivElement closeControlHitbox;
-
   PanelView(int id, int col, String refName, String fullName, String shortName, List config, [String externalCssPath]) :
   super(id, col, refName, fullName, shortName, config, querySelector('#column-$col').children[0]) {
     if (externalCssPath != null) {
@@ -22,16 +20,6 @@ class PanelView extends ContainerView {
     }
 
     tabHandle.classes.add('panel-handle');
-
     tabHandleButton.text = '$shortName';
-
-    closeControlHitbox = new DivElement()
-      ..title = 'Close'
-      ..classes.add('close-control-hitbox');
-    tabHandle.children.insert(0, closeControlHitbox);
-
-    DivElement closeControl = new DivElement()
-      ..classes.addAll(['close-control', 'glyphicons', 'glyphicons-remove-2']);
-    closeControlHitbox.children.add(closeControl);
   }
 }
