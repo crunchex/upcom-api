aa5d4aa94aa8058ece12eabfa5fa985a286d2a13
Mike Lewis <mike@updroid.com>
2015-08-10 14:20:16 -0700

    ros: moved list nodes method from workspace to static ros

--- lib/src/ros/ros.dart
+++ lib/src/ros/ros.dart
@@ -1,5 +1,6 @@
 library upcom_api.lib.src.ros.ros;
 
+import 'dart:async';
 import 'dart:io';
 
 /// A class for static ROS methods that don't require instantiation of
@@ -8,5 +9,22 @@ abstract class Ros {
   static void startRosCore() {
     String buildCommand = '/opt/ros/indigo/setup.bash && roscore';
     Process.start('bash', ['-c', '. $buildCommand'], runInShell: true);
+  }
+
+  /// Lists currently running nodes.
+  ///
+  /// Equivalent to running 'rosnode list'.
+  static Future<List> listRunningNodes() {
+    Completer c = new Completer();
+
+    String buildCommand = '/opt/ros/indigo/setup.bash && rosnode list';
+    Process.run('bash', ['-c', '. $buildCommand'], runInShell: true).then((ProcessResult result) {
+      List<String> nodesList = result.stdout.split('\n');
+      // Splitting on newline yields an empty element in the last spot.
+      nodesList.removeLast();
+      c.complete(nodesList);
+    });
+
+    return c.future;
   }
 }
