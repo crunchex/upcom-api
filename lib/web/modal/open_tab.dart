e558fba46386a5655f67177a5b4960de07decf46
don <don@updroid.com>
2015-08-26 20:29:20 -0700

    Shop demo settings.

--- lib/web/modal/open_tab.dart
+++ lib/web/modal/open_tab.dart
@@ -4,28 +4,63 @@ class UpDroidOpenTabModal extends UpDroidModal {
   Function _openTab;
   Map _tabsInfo;
 
-  UpDroidOpenTabModal(Function openTab, Map tabsInfo) {
+  UpDroidOpenTabModal(Function openTab, Map tabsInfo, {bool fake}) {
     _openTab = openTab;
     _tabsInfo = tabsInfo;
 
     _setupHead('Select Tab: ');
-    _setupBody();
+    if(fake != null) _setupFakeBody();
+    else {
+      _setupBody();
+    }
     _setupFooter();
 
     _showModal();
   }
 
   void _setupBody() {
+    print("real build");
     DivElement selectorWrap = new DivElement()
       ..id = "selector-wrapper";
     _modalBody.children.add(selectorWrap);
 
     _tabsInfo.values.forEach((Map tabInfo) {
       ButtonElement tabButton = _createButton('default', tabInfo['shortName'])..onClick.listen((_) => _openTab(tabInfo));
-      if(tabInfo['shortName'] == 'teleop') {
-        tabButton.classes.add('hidden', 'teleop');
+      if(tabInfo['shortName'] == "Shop") {
+        tabButton.id = "shop-button";
+        selectorWrap.children.add(tabButton);
       }
-      selectorWrap.children.add(tabButton);
+    });
+
+    _tabsInfo.values.forEach((Map tabInfo) {
+      ButtonElement tabButton = _createButton('default', tabInfo['shortName'])..onClick.listen((_) => _openTab(tabInfo));
+      if(tabInfo['shortName'] != "Shop") {
+        selectorWrap.children.add(tabButton);
+      }
+    });
+  }
+
+  void _setupFakeBody() {
+    print('fake build');
+    DivElement selectorWrap = new DivElement()
+      ..id = "selector-wrapper";
+    _modalBody.children.add(selectorWrap);
+
+    _tabsInfo.values.forEach((Map tabInfo) {
+      ButtonElement tabButton = _createButton('default', tabInfo['shortName'])
+        ..onClick.listen((_) => _openTab(tabInfo));
+      if (tabInfo['shortName'] == "Shop") {
+        tabButton.id = "shop-button";
+        selectorWrap.children.add(tabButton);
+      }
+    });
+
+    _tabsInfo.values.forEach((Map tabInfo) {
+      ButtonElement tabButton = _createButton('default', tabInfo['shortName'])
+        ..onClick.listen((_) => _openTab(tabInfo));
+      if (tabInfo['shortName'] != "Shop" && tabInfo['shortName'] != "Teleop") {
+        selectorWrap.children.add(tabButton);
+      }
     });
   }
 
